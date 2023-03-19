d168dd6c64a72cc6aa35933e6393e6591d2a9859
eugene <eugenesgj@gmail.com>
2023-03-19 23:54:58 +0800

    lint:fix

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -2,8 +2,8 @@ import { render } from "@testing-library/react";
 import App from "./App";
 
 describe("App", () => {
-	it("should work as expected", () => {
-		render(<App />);
-		expect(1 + 1).toBe(2);
-	});
+  it("should work as expected", () => {
+    render(<App />);
+    expect(1 + 1).toBe(2);
+  });
 });
--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	server: {
-		port: 3000,
-	},
-	plugins: [react(), eslint()],
+  server: {
+    port: 3000,
+  },
+  plugins: [react(), eslint()],
 });
