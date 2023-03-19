b3c2ea8eefa8171e4cd04cd1d07b791d7cc06d95
eugene <eugenesgj@gmail.com>
2023-03-20 00:11:36 +0800

    init prettier and react router

---
+++ src/pages/login.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from "react";
+
+const LoginPage: FunctionComponent = () => {
+  return (
+    <div>
+      <p>login page</p>
+    </div>
+  );
+};
+
+export default LoginPage;
